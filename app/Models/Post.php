aa9eef119751ce79a1d93bf431427bfdd6667b2d
Rago179 <omar.s.sanad@gmail.com>
2025-10-17 03:16:28 +0100

    Relationship models

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    protected $fillable = ['user_id','title','description','status'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
 }
